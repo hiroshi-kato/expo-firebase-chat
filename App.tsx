a3bc735efad938cbf616fa640ecf147a20562f60
mamedaifuku <hiroshikato1208@gmail.com>
2021-06-06 15:23:40 +0900

    react-native-gifted-chatでチャット画面を実装

--- App.tsx
+++ App.tsx
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React, { useCallback, useEffect, useState } from 'react';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { GiftedChat, IMessage } from 'react-native-gifted-chat';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+export default function App() {
+  const [messages, setMessages] = useState<IMessage[]>([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'react native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = useCallback((messages = []) => {
+    setMessages((prevMessages) => GiftedChat.append(prevMessages, messages));
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <GiftedChat
+        messages={messages}
+        placeholder='メッセージを入力してください…'
+        onSend={onSend}
+        user={{
+          _id: 1,
+          name: 'me',
+          avatar: 'https://placeimg.com/140/140/any',
+        }}
+      />
+      {Platform.OS === 'android' && <KeyboardAvoidingView behavior='padding' />}
+    </View>
+  );
+}
