dbb413f1775afd05143b73fa0c611928ca914458
mamedaifuku <hiroshikato1208@gmail.com>
2021-06-06 15:30:54 +0900

    チャット画面のコードをファイル分割

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import {
   View,
 } from 'react-native';
 import { GiftedChat, IMessage } from 'react-native-gifted-chat';
+import { ChatScreen } from './src/screens/ChatScreen';
 
 const styles = StyleSheet.create({
   container: {
@@ -15,40 +16,5 @@ const styles = StyleSheet.create({
 });
 
 export default function App() {
-  const [messages, setMessages] = useState<IMessage[]>([]);
-
-  useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: 'hello developer',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'react native',
-          avatar: 'https://placeimg.com/140/140/any',
-        },
-      },
-    ]);
-  }, []);
-
-  const onSend = useCallback((messages = []) => {
-    setMessages((prevMessages) => GiftedChat.append(prevMessages, messages));
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <GiftedChat
-        messages={messages}
-        placeholder='メッセージを入力してください…'
-        onSend={onSend}
-        user={{
-          _id: 1,
-          name: 'me',
-          avatar: 'https://placeimg.com/140/140/any',
-        }}
-      />
-      {Platform.OS === 'android' && <KeyboardAvoidingView behavior='padding' />}
-    </View>
-  );
+  return <ChatScreen />;
 }
